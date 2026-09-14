f34ef4e658fcec7f0810f745ae7f76e89f99524a
IBUKI MIODAAAA!!! <xavier.cunning2007@gmail.com>
2026-09-14 04:39:49 -0500

    added extend

--- common/src/main/java/net/hydra/jojomod/stand/powers/PowersKhnum.java
+++ common/src/main/java/net/hydra/jojomod/stand/powers/PowersKhnum.java
@@ -1,1 +1,72 @@
+package net.hydra.jojomod.stand.powers;
 
+import com.google.common.collect.Lists;
+import net.hydra.jojomod.Roundabout;
+import net.hydra.jojomod.access.IEntityAndData;
+import net.hydra.jojomod.block.InvisiBlockEntity;
+import net.hydra.jojomod.block.ModBlocks;
+import net.hydra.jojomod.block.RoundaboutAttackBlock;
+import net.hydra.jojomod.client.ClientNetworking;
+import net.hydra.jojomod.client.StandIcons;
+import net.hydra.jojomod.client.hud.StandHudRender;
+import net.hydra.jojomod.entity.stand.JusticeEntity;
+import net.hydra.jojomod.event.AbilityIconInstance;
+import net.hydra.jojomod.event.ModParticles;
+import net.hydra.jojomod.event.index.PowerIndex;
+import net.hydra.jojomod.event.index.SoundIndex;
+import net.hydra.jojomod.event.powers.CooldownInstance;
+import net.hydra.jojomod.event.powers.StandPowers;
+import net.hydra.jojomod.event.powers.StandUser;
+import net.hydra.jojomod.networking.ModPacketHandler;
+import net.hydra.jojomod.sound.ModSounds;
+import net.hydra.jojomod.stand.powers.elements.PowerContext;
+import net.hydra.jojomod.stand.powers.presets.NewDashPreset;
+import net.hydra.jojomod.util.MainUtil;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.*;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.item.context.DirectionalPlaceContext;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.block.ShulkerBoxBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.CollisionContext;
+
+import javax.annotation.Nullable;
+import java.util.List;
+public class PowersKhnum extends NewDashPreset {
+    public PowersKhnum(LivingEntity self) {
+        super(self);
+    }
+
+    @Override
+    public StandPowers generateStandPowers(LivingEntity entity) {
+        return new PowersKhnum(entity);
+    }
+    @Override
+    /**Override to add disable config*/
+    public boolean isStandEnabled(){
+        return ClientNetworking.getAppropriateConfig().khnumSettings.enableKhnum;
+    }
